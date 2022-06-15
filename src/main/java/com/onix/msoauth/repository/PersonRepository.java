fb20532523654008b6c3bcec90dd352f551b0e51
unknown <datkach77@gmail.com>
2022-06-15 23:36:53 +0300

    add pagination&sorting

--- src/main/java/com/onix/msoauth/repository/PersonRepository.java
+++ src/main/java/com/onix/msoauth/repository/PersonRepository.java
@@ -2,12 +2,15 @@ package com.onix.msoauth.repository;
 
 import com.onix.msoauth.entities.Level;
 import com.onix.msoauth.entities.Person;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
 
-public interface PersonRepository extends CrudRepository<Person, Integer> {
-    List<Person> findBySkillsContaining(String skills);
-    Iterable<Person> findByLevelAndSkillsContaining(Level level, String skills);
+public interface PersonRepository extends PagingAndSortingRepository<Person, Integer> {
+    Page<Person> findBySkillsContaining(String skills, Pageable pageable);
+    List<Person> findByLevelAndSkillsContaining(Level level, String skills);
 }
