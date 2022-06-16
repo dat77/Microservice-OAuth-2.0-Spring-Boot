7eb2d41f9c9ca860305c991236c6c551704c96f8
unknown <datkach77@gmail.com>
2022-06-16 12:18:05 +0300

    add controlling REST API

--- src/main/java/com/onix/msoauth/repository/PersonRepository.java
+++ src/main/java/com/onix/msoauth/repository/PersonRepository.java
@@ -7,10 +7,41 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.List;
 
+@RepositoryRestResource(collectionResourceRel = "employees", path = "employees")
 public interface PersonRepository extends PagingAndSortingRepository<Person, Integer> {
     Page<Person> findBySkillsContaining(String skills, Pageable pageable);
     List<Person> findByLevelAndSkillsContaining(Level level, String skills);
+
+    @Override
+    @RestResource(exported = true)
+    <S extends Person> S save(S entity);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends Person> Iterable<S> saveAll(Iterable<S> entities);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteById(Integer integer);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(Person entity);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAllById(Iterable<? extends Integer> integers);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll(Iterable<? extends Person> entities);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
 }
--- src/main/java/com/onix/msoauth/repository/ProjectTeamRepository.java
+++ src/main/java/com/onix/msoauth/repository/ProjectTeamRepository.java
@@ -5,13 +5,45 @@ import com.onix.msoauth.entities.ProjectTeam;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.Collection;
 import java.util.List;
 
+@RepositoryRestResource(collectionResourceRel = "teams", path = "teams")
 public interface ProjectTeamRepository extends CrudRepository<ProjectTeam, String> {
     Collection<ProjectTeam> findByStatus(ProjectStatus status);
     @Query("select t from ProjectTeam t where t.code like %?1% ")
     List<ProjectTeam> findByCodeLike(String code);
     List<ProjectTeam> findByCodeContaining(String code);
+
+
+    @Override
+    @RestResource(exported = true)
+    <S extends ProjectTeam> S save(S entity);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends ProjectTeam> Iterable<S> saveAll(Iterable<S> entities);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteById(String s);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(ProjectTeam entity);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAllById(Iterable<? extends String> strings);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll(Iterable<? extends ProjectTeam> entities);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
 }
